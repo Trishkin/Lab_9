a442f7efd78bd1b770961a956e8351b897eafdd8
Дмитрий Куликов <kulikov_dimochka@mail.ru>
2020-11-12 14:54:24 +0300

    Events and lambda

--- Lab_9/Models/Game.cs
+++ Lab_9/Models/Game.cs
@@ -4,13 +4,20 @@ using System.Text;
 
 namespace Lab_9.Models
 {
+    delegate int Operation(int x, int y);
     public class Game
     {
+        Operation operation = (x, y) => x - y;
+        Operation operation2 = (x, y) => x + y;
+
+        public delegate void GameHandler(string message);
+        public event GameHandler Notify;
+
         public readonly string _person;
         private int _damage;
         public int Hp;
         private int _heal;
-        public Game(string Person, int Damage, int HP, int Heal)
+        public Game(string Person, int HP, int Damage, int Heal)
         {
             _person = Person;
             _damage = Damage;
@@ -20,12 +27,19 @@ namespace Lab_9.Models
 
         public void Hit(Game enemy)
         {
-            enemy.Hp = enemy.Hp - this._damage;
+            enemy.Hp = operation(enemy.Hp , this._damage);
+            Notify?.Invoke($"{this._person} нанес {this._damage} урона {enemy._person}");
         }
 
         public void Healing(Game character)
         {
-            character.Hp = character.Hp + this._heal;
+            character.Hp = operation2(character.Hp , this._heal);
+            Notify?.Invoke($"{this._person} полечил {character._person} на {this._heal}");
+        }
+
+        public void Status()
+        {
+            Console.WriteLine($"Name = {_person}, HP = {Hp}, damage = {_damage}, heal = {_heal}");
         }
     }
 }
--- Lab_9/Program.cs
+++ Lab_9/Program.cs
@@ -1,12 +1,30 @@
-﻿using System;
+﻿using Lab_9.Models;
+using System;
 
 namespace Lab_9
 {
     class Program
     {
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Game You = new Game("Charly", 100, 25, 15);
+            You.Notify += DisplayMessage;
+            Game Enemy1 = new Game("Zombi", 50, 10, 5);
+            Game Enemy2 = new Game("Sceleton", 30, 15, 0);
+            Enemy1.Status();
+            You.Hit(Enemy1);
+            Enemy1.Status();
+            Console.WriteLine("_______________________________");
+            Enemy2.Status();
+            You.Healing(Enemy2);
+            Enemy2.Status();
+
+        }
+
+        private static void DisplayMessage(string message)
+        {
+            Console.WriteLine(message);
         }
     }
 }
